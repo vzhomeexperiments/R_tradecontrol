810db84a4a2d00e7dd5ff5ed65cf05f0feede687
Vladimir Zhbanko <vz.home.experiments@gmail.com>
2017-10-08 15:04:42 +0200

    Add files via upload

---
+++ 06_T1-3Task.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\R\R-3.2.5\bin\R.exe" CMD BATCH "C:\Users\fxtrams\Documents\00_FXTRAMS_2.0\03_AnalyticalCentre\01_TradesMonitoring\07_TradeAnalyser_T1_3_v7.2.R"
---
+++ 07_PerformanceAnalyser_T1_v1.1.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files\R\R-3.2.5\bin\R.exe" CMD BATCH "C:\Users\fxtrams\Documents\00_FXTRAMS_2.0\03_AnalyticalCentre\01_TradesMonitoring\07_PerformanceAnalyser_T1_v1.2.R"
