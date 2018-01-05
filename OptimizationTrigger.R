2f2e836451f26b1a5ca0a1370b0b5481aa833880
vzhomeexperiments <vz.home.experiments@gmail.com>
2018-01-05 19:21:51 +0100

    created practical task

--- OptimizationTrigger.R
+++ OptimizationTrigger.R
@@ -1,98 +1,26 @@
-# 20170822 
-# This is a dedicated script for 1st Terminal /Demo account/
-# Copyright (C)Vladimir Zhbanko 20170822
+## This is a dedicated script for the Lazy Trading 4th Course: Statistical Analysis of Trades
+# Copyright (C) 2018 Vladimir Zhbanko
 
-# Version 1.0 (started from 07_TradeAnalyser_T1_3_v7.2.R)
-#           1 (adding profit factor to the output table)
-#
+# PURPOSE: Analyse trade results in Terminal 1. Write a summary, indicate when to optimize systems
+# NOTE:    Results are written in the Trading System Version Control Repository. 
+#          Important: Trading System version control must contain a list of trading systems in use
 
-#  - **Main objective:** to output summary table of the systems that needs to be re-trained or re-optimized
-#  - LOGIC:
-# 1.	ON TERMINAL 1
-# 2.	Group by trading systems
-# 3.	analyse last 20 trades
-# 4.	get their profit factor
-# 5.	filter those systems where profit factor is lower than 1.0 write them to the table
-# 6.	run this analysis bi-weekly, every Saturday, etc
-# 7.	re-train only the systems that will pop up in the list...
-
-#  - Only apply to the systems currently under development!
-#  - List of systems under development...
-#  - Path to trading system project...
-
-# use only tidyverse package that contains all others...
+# load packages
 library(tidyverse)
+library(lubridate)
+library(readxl)
 
 # ----------- Main Steps -----------------
 # -- Read trading results from Terminal 1
-# -- Split trading results from Terminal 1 into categories
-# -- Depend on conditions allow trades in Terminal 2 and Terminal 4
-# -- Monitor trade results using profit factor on both Terminals when profitable trades continue during 10 trades
-# -- Start/Stop trades on Terminals at desired profit factor levels of the systems
+# -- Analyse results and filter systems where profit factor < 0.7
+# -- Write command 'to optimize' to the file
 
 # =============================================
 # *************Used Functions******************
 # =============================================
-#===============================
-# WRITE COMMAND VIA CSV FUNCTION
-#===============================
-# write a function that will write csv files to the folder with specified address
-writeCommandViaCSV <- function(x, pathToTerminal, fileName = "SystemControl"){
-  if(exists("x") == TRUE && nrow(x) != 0) {
-  for(i in 1:nrow(x))
-  {
-    # write the file for MQL4 usage
-    write.csv(x[i, ], file = paste(pathToTerminal, fileName, as.character(x[i, 1]), ".csv", sep = ""),
-              row.names = FALSE)
-  }
-  } 
-}
+# *** make sure to customize this path
+source("C:/Users/fxtrams/Documents/000_TradingRepo/R_tradecontrol/profitFactor.R")
 
-#===============================
-# PROFIT FACTOR FUNCTION
-#===============================
-# function that returns the profit factors of the systems
-#
-# x - data frame with orders
-#     df must contain MagicNumber and Profit columns!
-# numOrders - desired number of orders to base profit factor calculation
-# 
-#
-profitFactor <- function(x, numOrders){
-  # generate DF with only MagicNumbers when > 10 trades and all trades are losers
-  DF_L <- x %>%
-    group_by(MagicNumber) %>%
-    summarise(nOrders = n())%>%
-    filter(nOrders > numOrders)%>%
-    select(MagicNumber)%>%
-    # subset only rows that contans magic numbers from x
-    inner_join(x, by = "MagicNumber")%>%
-    group_by(MagicNumber)%>%
-    filter(Profit < 0) %>%
-    summarise(Loss = abs(sum(Profit)))
-  # generate DF with only MagicNumbers when > 10 trades and all trades are profits
-  DF_P <- x %>%
-    group_by(MagicNumber) %>%
-    summarise(nOrders = n())%>%
-    filter(nOrders > numOrders)%>%
-    select(MagicNumber)%>%
-    # subset only rows that contans magic numbers from x
-    inner_join(x, by = "MagicNumber")%>%
-    group_by(MagicNumber)%>%
-    filter(Profit > 0) %>%
-    summarise(Gain = abs(sum(Profit)))
-  # generate DF containing profit factor of all systems
-  DF <- DF_P %>%
-    full_join(DF_L, by = "MagicNumber")
-  # replace any NA with zeroes!
-  DF[is.na(DF)] <- 1
-  # calculate profit factors of the each system!
-  DF_PF <- DF%>%
-    group_by(MagicNumber)%>%
-    mutate(PrFact = Gain/(0.001+Loss))%>%
-    select(MagicNumber, PrFact)
-  return(DF_PF)
-}
 # =============================================
 # ************End of Used Functions************
 # =============================================
@@ -101,45 +29,45 @@ profitFactor <- function(x, numOrders){
 # Define terminals path addresses, from where we are going to read/write data
 # -------------------------
 # terminal 1 path
-path_T2 <- "C:/Program Files (x86)/FxPro - Terminal1/MQL4/Files/"
+path_T1 <- "C:/Program Files (x86)/FxPro - Terminal1/MQL4/Files/"
 # terminal 3 path
-path_T4 <- "C:/Program Files (x86)/FxPro - Terminal3/MQL4/Files/"
+path_T3 <- "C:/Program Files (x86)/FxPro - Terminal3/MQL4/Files/"
 # trading system project folder
-path_PRJCT <- "C:/Users/fxtrams/Documents/00_FXTRAMS_2.0/01_TradingStrategies/19_AI_TradeEuclidean/"
+path_PRJCT <- "C:/Users/fxtrams/Documents/000_TradingRepo/FALCON_B/"
 
 # -------------------------
 # read data from trades in terminal 2
 # -------------------------
-DFT2 <- try(read_csv(file = paste(path_T2, "OrdersResultsT1.csv", sep = ""), 
+DFT1 <- try(read_csv(file = paste(path_T1, "OrdersResultsT1.csv", sep = ""), 
                      col_names = c("MagicNumber", "TicketNumber", "OrderStartTime", 
                                    "OrderCloseTime", "Profit", "Symbol", "OrderType"),
                      col_types = "iiccdci"), silent = TRUE)
 
 # data frame preparation, function ymd_hms from 'lubridate' package
-DFT2$OrderStartTime <- lubridate::ymd_hms(DFT2$OrderStartTime)
-DFT2$OrderCloseTime <- lubridate::ymd_hms(DFT2$OrderCloseTime)
-DFT2$OrderType      <- as.factor(DFT2$OrderType)
+DFT1$OrderStartTime <- ymd_hms(DFT1$OrderStartTime)
+DFT1$OrderCloseTime <- ymd_hms(DFT1$OrderCloseTime)
+DFT1$OrderType      <- as.factor(DFT1$OrderType)
 
 
 # -------------------------
-# data frame T2 analysis and manipulation
+# data frame T1 analysis and manipulation
 # -------------------------
 # ----------------
 # Summarise orders acc.to the logic below
 #-----------------
 # 1.	ON TERMINAL 1
 # 2.	Group by trading systems
-# 3.	analyse last 20 trades --> data is in *DFT2_L*
+# 3.	analyse last 20 trades --> data is in *DFT1*
 # 4.	get their profit factor
-# 5.	filter those systems where profit factor is lower than 1.6 write them to the table
+# 5.	filter those systems where profit factor is lower than 0.7 
 # 6.	run this analysis weekly, every Friday
-# 7.	re-train only the systems 
+# 7.	re-train only those systems with lower factor
 # ----------------
 # Implementation of logic
 #-----------------
 #### SORTING AND DECIDE IF SYSTEM NEEDS TO BE RE-TRAINED/RE-OPTIMIZED #### -----------------------------
-# 4. Last 20 orders on DEMO && pr.fact < 1.0 SUGGEST TO RE-TRAINE           -> DFT2_1 
-DFT2_1 <- DFT2 %>%  # filtered to contain last 20 orders for each system
+# 4. Last 20 orders on DEMO && pr.fact < 0.7 SUGGEST TO RE-TRAINE
+DFT1 %>%  # filtered to contain last 20 orders for each system
   group_by(MagicNumber) %>% 
   arrange(MagicNumber, desc(OrderCloseTime)) %>% 
   filter(row_number() <= 21) %>% 
@@ -148,7 +76,7 @@ DFT2_1 <- DFT2 %>%  # filtered to contain last 20 orders for each system
   filter(PrFact < 0.7) %>% 
   select(MagicNumber, PrFact) %>% 
   mutate(ToOptimize = 1) %>% 
-  inner_join(y = readxl::read_excel(path = paste0(path_PRJCT, "Setup_20170703.xlsx")), by = c("MagicNumber" = "Magic")) %>% 
+  inner_join(y = read_excel(path = paste0(path_PRJCT, "Setup.xlsx")), by = c("MagicNumber" = "Magic")) %>% 
   write_csv(path = paste0(path_PRJCT, Sys.Date(), "-Re-Train", ".csv"))
 
 
--- OptimizationTrigger.bat
+++ OptimizationTrigger.bat
@@ -1,2 +1,5 @@
 @echo off
-"C:\Program Files\R\R-3.2.5\bin\R.exe" CMD BATCH "C:\Users\fxtrams\Documents\00_FXTRAMS_2.0\03_AnalyticalCentre\11_GitTest\tradecontrol\07_TradeAnalyser_T1_3_v7.2.R"
+:: Use this line of code for debugging purposes *** Adapt the paths for R installation and R script!!!
+::"C:\Program Files\R\R-3.4.3\bin\R.exe" CMD BATCH "C:\Users\fxtrams\Documents\000_TradingRepo\R_tradecontrol\OptimizationTrigger.R"
+:: Use this code in 'production'
+"C:\Program Files\R\R-3.4.3\bin\Rscript.exe" "C:\Users\fxtrams\Documents\000_TradingRepo\R_tradecontrol\OptimizationTrigger.R"
