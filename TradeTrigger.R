75e88f4b2a9c05db0127c2defd0a7408c9337571
vzhomeexperiments <vz.home.experiments@gmail.com>
2018-01-05 13:55:22 +0100

    clean code and separate functions

--- TradeTrigger.R
+++ TradeTrigger.R
@@ -1,24 +1,11 @@
-# 20161109 
-# This is a dedicated script for 2nd and 4th Terminals /Demo account/
-# Copyright (C)Vladimir Zhbanko 20170108
-# Version 06 (copy of Version05) just dedicated to 2 and 4 terminals 
-#  - managing both 2nd and 4th terminals within the same script
-#  - having a function that sends a commands (writing files) to the specified path
-#  - more robust programming methods implementation, using dplyr packages
-#  - real account is managed on the 10 last trades basis to protect against deterioration of the systems
-# Version 07 
-#  - including reading file to check for the news events and block trades on accounts
-# Version 07.1
-#  - stopping real accoun t if there are more than 2 consecutive losses
-# Version 7.2 to return to this use code in console: system("git checkout cde7d6ea")
-#  - version only account for last 10 trades in Demo terminal and demo system will always trade!
-# Version 7.2.1 to return to this use code in console: system("git checkout 93d1ffc8")
-#  - simplified logic and code
+# This is a dedicated script for the Course Statistical Analysis of your Trades
+# Copyright (C) 2018 Vladimir Zhbanko
+
+# PURPOSE: Analyse trade results in Terminal 1 and Trigger or Stop Trades in Terminal 3
 
 # packages used
 library(lubridate) #install.packages("lubridate")
-library(dplyr)
-library(readr)   #used to import data read_csv
+library(tidyverse) #install.packages("tidyverse")
 
 # ----------- Applied Logic -----------------
 # -- Read trading results from Terminal 1
@@ -32,125 +19,41 @@ library(readr)   #used to import data read_csv
 # ----------------
 # Used Functions
 #-----------------
-#===============================
-# WRITE COMMAND VIA CSV FUNCTION
-#===============================
-# write a function that will write csv files to the folder with specified address
-writeCommandViaCSV <- function(x, pathToTerminal, fileName = "SystemControl"){
-  if(exists("x") == TRUE && nrow(x) != 0) {
-  for(i in 1:nrow(x))
-  {
-    # write the file for MQL4 usage
-    write.csv(x[i, ], file = paste(pathToTerminal, fileName, as.character(x[i, 1]), ".csv", sep = ""),
-              row.names = FALSE)
-  }
-  } 
-}
-
-#===============================
-# PROFIT FACTOR FUNCTION
-#===============================
-# function that returns the profit factors of the systems
-#
-# x - data frame with orders
-#     df must contain MagicNumber and Profit columns!
-# numOrders - desired number of orders to base profit factor calculation
-# 
-#
-profitFactor <- function(x, numOrders){
-  # generate DF with only MagicNumbers when > 10 trades and all trades are losers
-  DF_L <- x %>%
-    group_by(MagicNumber) %>%
-    summarise(nOrders = n())%>%
-    filter(nOrders > numOrders)%>%
-    select(MagicNumber)%>%
-    # subset only rows that contans magic numbers from x
-    inner_join(x, by = "MagicNumber")%>%
-    group_by(MagicNumber)%>%
-    filter(Profit < 0) %>%
-    summarise(Loss = abs(sum(Profit)))
-  # generate DF with only MagicNumbers when > 10 trades and all trades are profits
-  DF_P <- x %>%
-    group_by(MagicNumber) %>%
-    summarise(nOrders = n())%>%
-    filter(nOrders > numOrders)%>%
-    select(MagicNumber)%>%
-    # subset only rows that contans magic numbers from x
-    inner_join(x, by = "MagicNumber")%>%
-    group_by(MagicNumber)%>%
-    filter(Profit > 0) %>%
-    summarise(Gain = abs(sum(Profit)))
-  # generate DF containing profit factor of all systems
-  DF <- DF_P %>%
-    full_join(DF_L, by = "MagicNumber")
-  # replace any NA with zeroes!
-  DF[is.na(DF)] <- 1
-  # calculate profit factors of the each system!
-  DF_PF <- DF%>%
-    group_by(MagicNumber)%>%
-    mutate(PrFact = Gain/(0.001+Loss))%>%
-    select(MagicNumber, PrFact)
-  return(DF_PF)
-}
+source("writeCommandViaCSV.R")
+source("profitFactor.R")
 
 # -------------------------
 # Define terminals path addresses, from where we are going to read/write data
 # -------------------------
 # terminal 1 path
-path_T2 <- "C:/Program Files (x86)/FxPro - Terminal1/MQL4/Files/"
+path_T1 <- "C:/Program Files (x86)/FxPro - Terminal1/MQL4/Files/"
 # terminal 3 path
-path_T4 <- "C:/Program Files (x86)/FxPro - Terminal3/MQL4/Files/"
+path_T3 <- "C:/Program Files (x86)/FxPro - Terminal3/MQL4/Files/"
 
 # -------------------------
 # read data from trades in terminal 1
 # -------------------------
-DFT2 <- try(read_csv(file = paste(path_T2, "OrdersResultsT1.csv", sep = ""), 
+DFT1 <- try(read_csv(file = paste(path_T1, "OrdersResultsT1.csv", sep = ""), 
                      col_names = c("MagicNumber", "TicketNumber", "OrderStartTime", 
                                    "OrderCloseTime", "Profit", "Symbol", "OrderType"),
                      col_types = "iiccdci"), silent = TRUE)
 
 # data frame preparation
-DFT2$OrderStartTime <- ymd_hms(DFT2$OrderStartTime)
-DFT2$OrderCloseTime <- ymd_hms(DFT2$OrderCloseTime)
-DFT2$OrderType      <- as.factor(DFT2$OrderType)
+DFT1$OrderStartTime <- ymd_hms(DFT1$OrderStartTime)
+DFT1$OrderCloseTime <- ymd_hms(DFT1$OrderCloseTime)
+DFT1$OrderType      <- as.factor(DFT1$OrderType)
 
 # get last 10 trades for each Magic system and arrange orders to have descending order
-DFT2_L <- DFT2 %>%  # filtered to contain last 10 orders for each system
+DFT1_L <- DFT1 %>%  # filtered to contain last 10 orders for each system
   group_by(MagicNumber) %>% 
   arrange(MagicNumber, desc(OrderCloseTime)) %>% 
   filter(row_number() <= 11)
 
-# get last 2 trades for each Magic system and arrange orders to have descending order
-DFT2_L2 <- DFT2 %>%  # filtered to contain last 10 orders for each system
+# get last 3 trades for each Magic system and arrange orders to have descending order
+DFT1_L2 <- DFT1 %>%  # filtered to contain last 10 orders for each system
   group_by(MagicNumber) %>% 
   arrange(MagicNumber, desc(OrderCloseTime)) %>% 
   filter(row_number() <= 3)
-# -------------------------
-# read data from trades in terminal 4
-# -------------------------
-#try(read.csv(file = paste(path_T4, "OrdersResultsT4.csv", sep = ""), header = FALSE), silent = TRUE)
-# use try to avoid error!!!
-DFT4 <- try(read_csv(file = paste(path_T4, "OrdersResultsT3.csv", sep = ""),
-                     col_names = c("MagicNumber", "TicketNumber", "OrderStartTime", 
-                                   "OrderCloseTime", "Profit", "Symbol", "OrderType"),
-                     col_types = "iiccdci"), silent = TRUE)
-                     
-# data frame preparation
-DFT4$OrderStartTime <- ymd_hms(DFT4$OrderStartTime)
-DFT4$OrderCloseTime <- ymd_hms(DFT4$OrderCloseTime)
-DFT4$OrderType      <- as.factor(DFT4$OrderType)
-
-# get last 15 trades for each Magic system and arrange orders to have descending order
-DFT4_L <- DFT4 %>%  # filtered to contain last 15 orders for each system
-  group_by(MagicNumber) %>% 
-  arrange(MagicNumber, desc(OrderCloseTime)) %>% 
-  filter(row_number() < 15)
-# get last 2 trades for each Magic system and arrange orders to have descending order
-DFT4_L2 <- DFT4 %>%  # filtered to contain last 2 orders for each system
-  group_by(MagicNumber) %>% 
-  arrange(MagicNumber, desc(OrderCloseTime)) %>% 
-  filter(row_number() <= 2)
-
 
 # -------------------------
 # data frame T1 analysis and manipulation
@@ -163,86 +66,99 @@ DFT4_L2 <- DFT4 %>%  # filtered to contain last 2 orders for each system
 # == Not enable Real account until > than 10 trades on Demo account were completed
 # == Enable Real account when > than 10 trades and profit factor in T1 is > 1.6
 # == Enable Real account when > than 2 trades and profit factor in T1 is > 1.2
-# == Not trade when there is a news macroscopic event
+# == Not trade when there is a news macroecon event
 
 # ----------------
 # Implementation of logic
 #-----------------
 #### SORTING AND DECIDE IF TRADING ON THE DEMO ACCOUNT #### -----------------------------
 # DEMO always allow trades                               
-DFT2_L %>%
+DFT1_L %>%
   group_by(MagicNumber) %>%
   summarise(nOrders = n()) %>%
-  #filter(nOrders <= 10)%>%
   select(MagicNumber) %>%
   mutate(IsEnabled = 1) %>% 
   # Write command "allow"
-  writeCommandViaCSV(path_T2)
+  writeCommandViaCSV(path_T1)
 # ======================= OK
 
-#### DECIDE IF TRADING ON THE REAL ACCOUNT #### -----------------------------
-# Last 10 orders on DEMO && pr.fact > 1.6 start trade REAL         
-DFT2_L %>%
+#### DECIDE IF TRADING ON THE T3 ACCOUNT #### -----------------------------
+# Last 10 orders on DEMO && pr.fact > 1.6 start trade T3
+DFT1_L %>%
   profitFactor(10) %>% 
   ungroup() %>% 
   filter(PrFact >= 1.6) %>% 
   select(MagicNumber) %>% 
   mutate(MagicNumber = MagicNumber + 200, IsEnabled = 1) %>% 
   # Write command "allow"
-  writeCommandViaCSV(path_T4)
+  writeCommandViaCSV(path_T3)
 
-# 4. Last 2 orders on DEMO && pr.fact > 1.2 trade REAL          
-DFT2_L2 %>%
+# 4. Last 2 orders on DEMO && pr.fact > 1.2 trade T3         
+DFT1_L2 %>%
   profitFactor(2) %>% 
   ungroup() %>% 
   filter(PrFact >= 1.2) %>% 
   select(MagicNumber) %>% 
   mutate(MagicNumber = MagicNumber + 200, IsEnabled = 1) %>% 
   # Write command "allow"
-  writeCommandViaCSV(path_T4)
+  writeCommandViaCSV(path_T3)
 
-#### DECIDE IF NOT TO TRADING ON THE REAL ACCOUNT #### -----------------------------
-# 4. Last 9 orders on DEMO && pr.fact < 1.6 start trade REAL
-DFT2_L %>%
+#### DECIDE IF NOT TO TRADING ON THE T3 ACCOUNT #### -----------------------------
+# 4. Last 10 orders on DEMO && pr.fact < 1.6 stop trade T3
+DFT1_L %>%
   profitFactor(10) %>% 
   ungroup() %>% 
   filter(PrFact < 1.6) %>% 
   select(MagicNumber) %>% 
   mutate(MagicNumber = MagicNumber + 200, IsEnabled = 0) %>% 
   # Write command "allow"
-  writeCommandViaCSV(path_T4)
+  writeCommandViaCSV(path_T3)
 
-# 4. Last 9 orders on DEMO && pr.fact < 1.2 stop trade REAL
-DFT2_L2 %>%
+# 4. Last 2 orders on DEMO && pr.fact < 1.2 stop trade T3
+DFT1_L2 %>%
   profitFactor(2) %>% 
   ungroup() %>% 
   filter(PrFact < 1.2) %>% 
   select(MagicNumber) %>% 
   mutate(MagicNumber = MagicNumber + 200, IsEnabled = 0) %>% 
   # Write command "allow"
-  writeCommandViaCSV(path_T4)
+  writeCommandViaCSV(path_T3)
 
 
 ##========================================
+# read data from trades in terminal 3
+# -------------------------
+#try(read.csv(file = paste(path_T3, "OrdersResultsT3.csv", sep = ""), header = FALSE), silent = TRUE)
+# use try to avoid error!!!
+DFT3 <- try(read_csv(file = paste(path_T3, "OrdersResultsT3.csv", sep = ""),
+                     col_names = c("MagicNumber", "TicketNumber", "OrderStartTime", 
+                                   "OrderCloseTime", "Profit", "Symbol", "OrderType"),
+                     col_types = "iiccdci"), silent = TRUE)
+
+# data frame preparation
+DFT3$OrderStartTime <- ymd_hms(DFT3$OrderStartTime)
+DFT3$OrderCloseTime <- ymd_hms(DFT3$OrderCloseTime)
+DFT3$OrderType      <- as.factor(DFT3$OrderType)
+
 # stopping all systems when macroeconomic event is present
-DF_NT <- read_csv(file= file.path(path_T2, "01_MacroeconomicEvent.csv"), col_types = "i")
+DF_NT <- read_csv(file= file.path(path_T1, "01_MacroeconomicEvent.csv"), col_types = "i")
 if(DF_NT[1,1] == 1) {
   # disable trades
-  DF_DisableT2 <- DFT2 %>% 
+  DF_DisableT1 <- DFT1 %>% 
     group_by(MagicNumber) %>% select(MagicNumber) %>% mutate(IsEnabled = 0)
-  DF_DisableT4 <- DFT4 %>% 
+  DF_DisableT3 <- DFT3 %>% 
     group_by(MagicNumber) %>% select(MagicNumber) %>% mutate(IsEnabled = 0)
   
-  writeCommandViaCSV(DF_DisableT2, path_T2)
-  writeCommandViaCSV(DF_DisableT4, path_T4)
+  writeCommandViaCSV(DF_DisableT1, path_T1)
+  writeCommandViaCSV(DF_DisableT3, path_T3)
 }
 
 
 ##========================================
-## write a summary T2 and T4
-# Terminal 2
+## write a summary T1 and T3
+# Terminal 1
 # Summary results
-DFT2 %>%
+DFT1 %>%
   group_by(MagicNumber) %>%
   summarise(SumProfit = sum(Profit),
             nOrders = n()) %>% 
@@ -250,12 +166,12 @@ DFT2 %>%
   write.csv(file = paste(path_T2, "SystemSummary", ".csv", sep = ""),
             row.names = FALSE)
 
-# Terminal 4
+# Terminal 3
 # Summary results
-DFT4%>%
+DFT3%>%
   group_by(MagicNumber) %>%
   summarise(SumProfit = sum(Profit),
             nOrders = n()) %>% 
   # create a summary file for review
-  write.csv(file = paste(path_T4, "SystemSummary", ".csv", sep = ""),
+  write.csv(file = paste(path_T3, "SystemSummary", ".csv", sep = ""),
           row.names = FALSE)
