c68cf810d208c301e68f340274c6b07f9d7dbb3c
vzhomeexperiments <vz.home.experiments@gmail.com>
2019-03-07 16:18:24 +0100

    simplified logic to stop/enable trades related to Macroeconomic events

--- _RL_MT/TradeTriggerRL_MT.R
+++ _RL_MT/TradeTriggerRL_MT.R
@@ -157,39 +157,44 @@ for (i in 1:length(vector_systems)) {
 # this will be covered in the Course #5 of the Lazy Trading Series!
 # -------------------------
 if(file.exists(file.path(path_T1, "01_MacroeconomicEvent.csv"))){
+  #read the file containing a flag (1 will mean that event is present hence no new opened orders)
   DF_NT <- read_csv(file= file.path(path_T1, "01_MacroeconomicEvent.csv"), col_types = "i")
+  #read the table of trading robots in operation
+  DF_Setup <- read_csv("C:/Users/fxtrams/Documents/000_TradingRepo/FALCON_A/TEST/Setup.csv")
+  
+  ## condition to disable systems
   if(DF_NT[1,1] == 1) {
-    # disable trades
-    if(!class(DFT1)[1]=='try-error'){
-      DFT1 %>%
-        group_by(MagicNumber) %>% select(MagicNumber) %>% mutate(IsEnabled = 0) %>% 
-        # write commands to disable systems
-        writeCommandViaCSV(path_T1)}
-    if(!class(DFT3)[1]=='try-error'){
-      DFT3 %>%
-        group_by(MagicNumber) %>% select(MagicNumber) %>% mutate(IsEnabled = 0) %>% 
-        writeCommandViaCSV(path_T3)}
+    # disable trades in T1
+    DF_Setup %>%
+      group_by(Magic) %>% select(Magic) %>% mutate(IsEnabled = 0) %>% 
+      # write commands to disable systems
+      writeCommandViaCSV(path_T1)
     
-    
+    # disable trades in T3
+    DF_Setup %>% group_by(Magic) %>% 
+      mutate(MagicNumber = Magic + 200, IsEnabled = 0) %>% 
+      group_by(MagicNumber) %>% 
+      select(MagicNumber, IsEnabled) %>% 
+      # write commands to disable systems
+      writeCommandViaCSV(path_T3)
+  
   }
-  # enable systems of T1 in case they were disabled previously
+  
+  ## condition to enable systems
   if(DF_NT[1,1] == 0) {
-    # enable trades of T1
-    if(!class(DFT1)[1]=='try-error'){
-      # temporary solution: enable trades from the working project
-      read_csv("C:/Users/fxtrams/Documents/000_TradingRepo/FALCON_A/TEST/Setup.csv") %>%
-        group_by(Magic) %>% select(Magic) %>% mutate(IsEnabled = 1) %>% 
-        # write commands to disable systems
-        writeCommandViaCSV(path_T1)
-      # temporary solution: enable trades from the working project
-      read_csv("C:/Users/fxtrams/Documents/000_TradingRepo/FALCON_A/TEST/Setup.csv") %>%
-        group_by(Magic) %>% 
-        mutate(MagicNumber = Magic + 200, IsEnabled = 1) %>% 
-        group_by(MagicNumber) %>% 
-        select(MagicNumber, IsEnabled) %>% 
-        # write commands to disable systems
-        writeCommandViaCSV(path_T3)}
+    # disable trades in T1
+    DF_Setup %>%
+      group_by(Magic) %>% select(Magic) %>% mutate(IsEnabled = 1) %>% 
+      # write commands to disable systems
+      writeCommandViaCSV(path_T1)
     
+    # disable trades in T3
+    DF_Setup %>% group_by(Magic) %>% 
+      mutate(MagicNumber = Magic + 200, IsEnabled = 1) %>% 
+      group_by(MagicNumber) %>% 
+      select(MagicNumber, IsEnabled) %>% 
+      # write commands to disable systems
+      writeCommandViaCSV(path_T3)
     
   }
   
