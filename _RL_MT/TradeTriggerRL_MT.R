1b08598ec3121d8fb91d2e1a4b1a31bf7de4e3aa
vzhomeexperiments <vz.home.experiments@gmail.com>
2019-04-08 06:32:06 +0200

    added function to evaluate macroeconomic event

--- _RL_MT/TradeTriggerRL_MT.R
+++ _RL_MT/TradeTriggerRL_MT.R
@@ -48,7 +48,7 @@ source("C:/Users/fxtrams/Documents/000_TradingRepo/R_tradecontrol/import_data_mt
 source("C:/Users/fxtrams/Documents/000_TradingRepo/R_tradecontrol/_RL_MT/generate_RL_policy.R")
 source("C:/Users/fxtrams/Documents/000_TradingRepo/R_tradecontrol/_RL_MT/record_policy.R")
 source("C:/Users/fxtrams/Documents/000_TradingRepo/R_tradecontrol/writeCommandViaCSV.R")
-
+source("C:/Users/fxtrams/Documents/000_TradingRepo/R_tradecontrol/evaluate_macroeconomic_event.R")
 # -------------------------
 # Define terminals path addresses, from where we are going to read/write data
 # -------------------------
@@ -60,6 +60,11 @@ path_T3 <- "C:/Program Files (x86)/FxPro - Terminal3/MQL4/Files/"
 
 # path where to read control parameters from
 path_control_files = "C:/Users/fxtrams/Documents/000_TradingRepo/R_tradecontrol/_RL_MT/control"
+
+# evaluate data on macroeconomic event (required to start trading)
+evaluate_macroeconomic_event(setup_path = "C:/Users/fxtrams/Documents/000_TradingRepo/FALCON_F2/TEST/Setup.csv",
+                             file_name = "01_MacroeconomicEvent.csv",
+                             path_t1 = path_T1,path_t3 = path_T3)
 # -------------------------
 # read data from trades in terminal 1
 # -------------------------
@@ -72,6 +77,9 @@ DFT1 <- try(import_data(path_T1, "OrdersResultsT1.csv"), silent = TRUE)
 # read data from trades in terminal 3
 # -------------------------
 DFT3 <- try(import_data(path_T3, "OrdersResultsT3.csv"), silent = TRUE)
+
+# check if we have no errors importing trading results
+if(!class(DFT1)[1]=="try-error"){
 
 # Vector with unique Trading Systems
 vector_systems <- DFT1 %$% MagicNumber %>% unique() %>% sort()
@@ -147,55 +155,7 @@ for (i in 1:length(vector_systems)) {
 }
 ### ============== END of FOR EVERY TRADING SYSTEM ###
 
+} # ============== END of condition to check  if(!class(DFT1_MT)[1]=="try-error")
 
 
 
-
-##========================================
-# -------------------------
-# stopping all systems when macroeconomic event is present
-# this will be covered in the Course #5 of the Lazy Trading Series!
-# -------------------------
-if(file.exists(file.path(path_T1, "01_MacroeconomicEvent.csv"))){
-  #read the file containing a flag (1 will mean that event is present hence no new opened orders)
-  DF_NT <- read_csv(file= file.path(path_T1, "01_MacroeconomicEvent.csv"), col_types = "i")
-  #read the table of trading robots in operation
-  DF_Setup <- read_csv("C:/Users/fxtrams/Documents/000_TradingRepo/FALCON_F2/TEST/Setup.csv")
-  
-  ## condition to disable systems
-  if(DF_NT[1,1] == 1) {
-    # disable trades in T1
-    DF_Setup %>%
-      group_by(Magic) %>% select(Magic) %>% mutate(IsEnabled = 0) %>% 
-      # write commands to disable systems
-      writeCommandViaCSV(path_T1)
-    
-    # disable trades in T3
-    DF_Setup %>% group_by(Magic) %>% 
-      mutate(MagicNumber = Magic + 200, IsEnabled = 0) %>% 
-      group_by(MagicNumber) %>% 
-      select(MagicNumber, IsEnabled) %>% 
-      # write commands to disable systems
-      writeCommandViaCSV(path_T3)
-  
-  }
-  
-  ## condition to enable systems
-  if(DF_NT[1,1] == 0) {
-    # disable trades in T1
-    DF_Setup %>%
-      group_by(Magic) %>% select(Magic) %>% mutate(IsEnabled = 1) %>% 
-      # write commands to disable systems
-      writeCommandViaCSV(path_T1)
-    
-    # disable trades in T3
-    DF_Setup %>% group_by(Magic) %>% 
-      mutate(MagicNumber = Magic + 200, IsEnabled = 1) %>% 
-      group_by(MagicNumber) %>% 
-      select(MagicNumber, IsEnabled) %>% 
-      # write commands to disable systems
-      writeCommandViaCSV(path_T3)
-    
-  }
-  
-}
