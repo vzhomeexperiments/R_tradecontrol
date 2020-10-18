4f6dbae47afef3412243d74109167049b4f1f959
vzhomeexperiments <vz.home.experiments@gmail.com>
2020-10-18 22:13:14 +0200

    update R version

--- _RL_MT/Adapt_RL_MT_control.bat
+++ _RL_MT/Adapt_RL_MT_control.bat
@@ -2,4 +2,4 @@
 :: Use this line of code for debugging purposes *** Adapt the paths for R installation and R script!!!
 ::"C:\Program Files\R\R-4.0.2\bin\R.exe" CMD BATCH "C:\Users\fxtrams\Documents\000_TradingRepo\R_tradecontrol\_RL_MT\Adapt_RL_MT_control.R"
 :: Use this code in 'production'
-"C:\Program Files\R\R-4.0.2\bin\Rscript.exe" "C:\Users\fxtrams\Documents\000_TradingRepo\R_tradecontrol\_RL_MT\Adapt_RL_MT_control.R"
+"C:\Program Files\R\R-4.0.3\bin\Rscript.exe" "C:\Users\fxtrams\Documents\000_TradingRepo\R_tradecontrol\_RL_MT\Adapt_RL_MT_control.R"
